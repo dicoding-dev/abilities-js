a74a340879bb09f2da7838c469d059de1906dd42
AlexzPurewoko <purwoko908@gmail.com>
2023-12-19 11:36:28 +0700

    add rule and its test

---
+++ src/abilities/core/objects/rule.test.js
@@ -0,0 +1,55 @@
+import { describe, it, test, expect } from "vitest";
+import { Action } from "./action";
+import { Resource } from "./resource";
+import { Scope } from "./scope";
+import { Rule } from "./rule";
+
+function makeRule({
+    resource,
+    inverted
+} = {resource: new Resource('resource'), inverted: false}) {
+    return new Rule(
+        new Scope('scope'),
+        resource,
+        new Action('action'),
+        inverted
+    );
+}
+
+describe("getRuleId function test", () => {
+    it("must throw error when not calling setRuleId beforehand", () => {
+        expect(() => makeRule().getRuleId()).toThrowError("Don't forget to call 'setRuleId()' before get the rule ID");
+    });
+
+    it("returns expected ruleId", () => {
+        const rule = makeRule();
+        rule.setRuleId(1);
+
+        expect(rule.getRuleId()).toBe(1);
+    });
+});
+
+describe("toString function test", () => {
+    test("successfully encode inverted rules", () => {
+        const rule = makeRule({
+            resource: new Resource('resource'),
+            inverted:  true
+        });
+        expect(`${rule}`).toEqual("!scope:resource/*:action");
+    });
+    test("successfully encode normal rules", () => {
+        const rule = makeRule();
+        expect(`${rule}`).toEqual("scope:resource/*:action");
+    });
+    test("successfully rule with custom resource", () => {
+        const rule = makeRule({
+            resource :   new Resource(
+                'res',
+                {
+                    'param_1' : 22
+                }
+            )
+            });
+        expect(`${rule}`).toEqual("scope:res/{\"param_1\":22}:action");
+    });
+});
