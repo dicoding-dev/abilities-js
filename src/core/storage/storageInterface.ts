163bde40033f4df4b9c114305da747e3660081a3
AlexzPurewoko <purwoko908@gmail.com>
2023-12-20 14:44:40 +0700

    import storage interface

---
+++ src/core/storage/storageInterface.ts
@@ -0,0 +1,11 @@
+export abstract class StorageInterface {
+
+    /**
+     * This method called at initial, or any changes made after 'commit()' called.
+     * It will retrieve all rules by specific userIdentification.
+     *
+     * @returns {(string[]|object[])} Can be a list of object that contains id and rule property.
+     *                  Or, it can be a list of rule string (read only mode).
+     */
+    abstract onGetRulesByUserId(userIdentification: number|string): string[]|object[]
+}
