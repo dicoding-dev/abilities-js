ea48f79f03256bb7a087d1ee822288d37bc9da15
AlexzPurewoko <purwoko908@gmail.com>
2023-12-20 14:55:57 +0700

    import: add index.ts for importing

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+
+import { Scope, Resource, Action, Rule, CompiledRules } from "./core/objects";
+
+import { RuleCompiler } from "./core/compiler/ruleCompiler";
+import { AbilityChecker } from "./core/comparator/abilityChecker";
+import { StorageInterface } from "./core/storage/storageInterface";
+import { ReadOnlyAbilityRepository } from "./core/repository/readOnlyAbilityRepository";
+
+export {
+    Scope, Resource, Action, Rule, CompiledRules,
+    RuleCompiler,
+    AbilityChecker,
+    StorageInterface,
+    ReadOnlyAbilityRepository
+}
