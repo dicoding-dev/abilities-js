d3cdff57b0edf6d986e37382423ceadd2a6ab1eb
AlexzPurewoko <purwoko908@gmail.com>
2023-12-20 09:55:49 +0700

    add index

---
+++ src/abilities/core/index.js
@@ -0,0 +1,12 @@
+import './comparator/abilityChecker';
+import './compiler/ruleCompiler';
+
+// objects
+import './objects/action';
+import './objects/scope';
+import './objects/resource';
+import './objects/compiledRules';
+import './objects/rule';
+
+import './storage/storageInterface';
+import './repository/readOnlyAbilityRepository';
