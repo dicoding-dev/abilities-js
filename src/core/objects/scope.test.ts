6e305b0d812a6308d8a5c567f215acf8d98668f3
AlexzPurewoko <purwoko908@gmail.com>
2023-12-26 22:37:43 +0700

    add scope name validation test

--- src/core/objects/scope.test.ts
+++ src/core/objects/scope.test.ts
@@ -5,6 +5,12 @@ it("must throw error when passing empty/blank argument on 'scope' ", () => {
     expect(() => new Scope('  ')).toThrowError('Scope must not be empty')
 });
 
+it("must throw error when passing invalid scope name", () => {
+    expect(() => new Scope('scope!')).toThrowError(
+        'Invalid scope naming. Please use a combination of lowercase letter, number, dash and underscore only'
+    );
+});
+
 test("Must use 'global' when using default argument", () => {
     expect((new Scope()).get()).toBe('global');
 });
